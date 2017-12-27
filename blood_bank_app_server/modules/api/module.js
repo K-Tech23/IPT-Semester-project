4bc2960aa6f41410c03ff7465fa2af917b8441c9
Mohammad-Khalid23 <m.k.ayub23@gmail.com>
2017-12-27 14:04:53 +0500

    first commit

---
+++ blood_bank_app_server/modules/api/module.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var main = require("./main/main")
+
+
+router.get('/', main.home);
+// router.get('*', main.notFound);
+
+/**blood bank request handlers**/
+router.get('/allBloodBankUser/:user', main.allBloodBankUsers);
+router.get('/bloodBankUser/:id', main.bloodBankUser);
+router.delete('/deleteBloodBankUser/:id', main.deleteBloodBankUser);
+router.put('/editBloodBankUser/:id', main.editBloodBankUsers);
+
+/**signup login request handlers**/
+router.post('/signup', main.signup);
+router.post('/login', main.login);
+
+
+
+module.exports = router;
